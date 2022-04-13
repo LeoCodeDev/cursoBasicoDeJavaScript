f11df4246229a79b7e3bea6053632480fd7a94db
code-from-scratch1 <alb.mar.cta@gmail.com>
2022-04-12 19:46:28 -0500

    clase 12 de javascript

---
+++ if.js
@@ -0,0 +1,9 @@
+var edad = 17;
+
+if(edad === 18){
+    document.write("Puedes votar, sera tu primera votacion")
+}else if(edad > 18){
+    document.write("Puedes votar de nuevo")
+}else{
+    document.write("No puedes votar")
+}
