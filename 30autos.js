4d430a20d94b62a1abaabd6ee65faeeb001e1beb
code-from-scratch1 <alb.mar.cta@gmail.com>
2022-04-14 16:12:35 -0500

    funcion constructora para crear lista de 30 vehiculos automaticamente

---
+++ 30autos.js
@@ -0,0 +1,30 @@
+let brands = ['Ford', 'Chevrolet', 'Fiat'];
+let models = ['GT', 'Utilitary', 'Van', 'Cargo', 'Compact']
+let cars   = [];
+let randomBrand = rand(0, 2);
+let randomModel = rand(0, 4);
+let randomYear  = rand(0, 27);
+
+function rand(a, b){
+    let ran = Math.floor(Math.random() * (a - b + 1)) + b;
+    return ran;
+}
+
+function car(brand, model, year){
+    this.brand = brand;
+    this.model = model;
+    this.year  = year;
+}
+
+for(i = 0 ; i < 30; i++){
+    let randomBrand = rand(0, 3);
+    let randomModel = rand(0, 5);
+    let randomYear  = rand(0, 27);
+    function rand(a, b){
+        let ran = Math.floor(Math.random() * (a - b)) + b;
+        return ran;
+    }
+    cars.push( new car(brands[randomBrand], models[randomModel], 1995 + randomYear));
+}
+
+console.log(cars);
